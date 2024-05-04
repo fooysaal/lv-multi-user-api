6d17d3eb12dca23a5e87f9d18ce2928ec0fd3c2e
foysal <mohibullah2k0@gmail.com>
2024-05-04 13:28:52 +0600

    admin api routes modified

--- routes/api.php
+++ routes/api.php
@@ -15,18 +15,20 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
     //route to add user types
-    Route::get('/user-types', [UserTypeController::class, 'index']);
-    Route::post('/user-types', [UserTypeController::class, 'store']);
-    Route::get('/user-types/{userType}', [UserTypeController::class, 'show']);
-    Route::put('/user-types/{userType}', [UserTypeController::class, 'update']);
-    Route::delete('/user-types/{userType}', [UserTypeController::class, 'destroy']);
-    Route::put('/user-types/{userType}/restore', [UserTypeController::class, 'restore']);
-    Route::delete('/user-types/{userType}/force-delete', [UserTypeController::class, 'forceDelete']);
+    Route::group(['middleware' => 'admin'], function () {
+        Route::get('/user-types', [UserTypeController::class, 'index']);
+        Route::post('/user-types', [UserTypeController::class, 'store']);
+        Route::get('/user-types/{id}', [UserTypeController::class, 'show']);
+        Route::put('/user-types/{id}', [UserTypeController::class, 'update']);
+        Route::delete('/user-types/{id}', [UserTypeController::class, 'destroy']);
+        Route::put('/user-types/{id}/restore', [UserTypeController::class, 'restore']);
+        Route::delete('/user-types/{id}/force-delete', [UserTypeController::class, 'forceDelete']);    
+        
+        //route to add user by admin
+        Route::post('/create-user', [AuthController::class, 'register']);
+    });
 
     //route to show profile and update profile
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::put('update-profile', [AuthController::class, 'updateProfile']);
-    
-    //route to add user by admin
-    Route::post('/create-user', [AuthController::class, 'register'])->middleware('admin');
 });
