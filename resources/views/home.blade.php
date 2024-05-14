cdcb5ba9df1938cda4632f0282e8564f61e87353
foysal <mohibullah2k0@gmail.com>
2024-05-14 19:14:33 +0600

    layouts view modified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if (Session::has('status'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ Session::get('status') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             <div class="card bg-dark">
                 <div class="card-header text-light">{{ __('Dashboard') }}</div>
-
+            
                 {{-- show registered user with out the logged in user --}}
                 <div class="card-body">
                     <table class="table table-dark table-striped">
@@ -18,6 +24,10 @@
                                 <th scope="col">Name</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">User Type</th>
+                                <th scope="col">Created By</th>
+                                @if(Auth::user()->isAdmin())
+                                <th scope="col">Actions</th>
+                                @endif
                             </tr>
                         </thead>
                         <tbody>
@@ -27,6 +37,18 @@
                                     <td>{{ $user->username }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>{{ $user->userType->name }}</td>
+                                    <td>{{ $user->created_by }}</td>
+                                    <td>
+                                        <a href="" class="btn btn-primary">View</a>
+                                        @if($user->created_by == Auth::user()->username)
+                                            <a href="" class="btn btn-warning">Edit</a>
+                                            <form action="" method="POST" style="display: inline-block;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        @endif
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
